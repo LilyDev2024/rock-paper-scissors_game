3e328dae7f8bc7919276f74c0617d3d07d41091f
lilitkravchenko <lilya.kravchenko@petcube.com>
2026-03-22 18:08:01 +0200

    Add function for getting computer choice

---
+++ game.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+
+
+function getComputerChoice(){
+    valueFrom0To1 = Math.random()
+    console.log(valueFrom0To1)
+    if (valueFrom0To1 <= 0.3){
+        return "rock"
+    } else if (valueFrom0To1 > 0.3 && valueFrom0To1 <= 0.6){
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
